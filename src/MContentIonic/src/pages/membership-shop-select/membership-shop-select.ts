e34bbe8920abd1eddb35b99e3bd377e69e3d55e3
Anawat <anawat@perfenterprise.com>
2019-01-17 17:37:23 +0700

    add merchant-model and binding membership-shop-select
    Signed-off-by: Anawat <anawat@perfenterprise.com>

--- src/MContentIonic/src/pages/membership-shop-select/membership-shop-select.ts
+++ src/MContentIonic/src/pages/membership-shop-select/membership-shop-select.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { MerchantResponse, Merchant } from '../../providers/merchant-model/merchant-model';
 
 @IonicPage()
 @Component({
@@ -7,7 +8,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'membership-shop-select.html',
 })
 export class MembershipShopSelectPage {
-
+  list: MerchantResponse[] = [];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    this.list = [
+      {
+        merchant: {
+          _id: "1",
+          logo: "assets/imgs/logo.png",
+          name: "Promome"
+        },
+        memberCount: 2546
+      },
+      {
+        merchant: {
+          _id: "2",
+          logo: "assets/imgs/logo.png",
+          name: "P2P Loan"
+        },
+        memberCount: 1345
+      },
+    ];
   }
 }
